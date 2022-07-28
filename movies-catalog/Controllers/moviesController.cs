ba5201945a56657856c413270761a25daec48898
Khiem <khiem17204@gmail.com>
2022-07-28 14:27:18 +0700

    alo

---
+++ movies-catalog/Controllers/moviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace movies_catalog.Controllers
+{
+    public class moviesController : Controller
+    {
+        // GET: movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
